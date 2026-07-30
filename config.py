47fc70f2d1832975602fa0fafdbda299ab38cb7c
dayo28056-art <dayo28056@gmail.com>
2026-07-30 14:58:41 -0600

    Update config.py

--- config.py
+++ config.py
@@ -9,18 +9,5 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 # Image file name
 IMAGE_FILE = "welcome.jpg"
 
-# Welcome message with your new functions
-WELCOME_TEXT = """✅ VIP has increased to 3.5% + 3📌
-
-🪙 REGISTER HERE ⏩⏩
-https://app-web.mobiuspe-app.com/regist?code=earnmoney426
-
-✅ We offer team leader salaries and up to 0.6% team commission. Please contact us to apply for a team leader position. 🛒
-
-Official channel link ⭐️
-https://t.me/mobiuspayofficial1
-
-Contact support ⭐️ @puya1521"""
-
-# Bot description (old functions)
+# Bot description
 BOT_DESCRIPTION = """🧰 CopyBot | Cleans text spaces, strips link trackers, and counts ad characters instantly. Tap /start!"""
